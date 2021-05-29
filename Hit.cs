a6fabc39dc50d4ab53079152532e4f1a46ebbf5c
taiyou1116 <hiromacha1116@icloud.com>
2021-05-29 20:56:52 +0900

    added space

--- Hit.cs
+++ Hit.cs
@@ -7,14 +7,14 @@ public class Hit : MonoBehaviour
     public Fungus.Flowchart flowchart = null;
     public string sendMessage = "";
     public string sendMessage2 = "";
-   
-   void OnCollisionEnter2D(Collision2D other)
-   {
-       if(other.gameObject.CompareTag("Player"))
-       {
-           
-           flowchart.SendFungusMessage(sendMessage);
-           flowchart.SendFungusMessage(sendMessage2);
-       }
-   }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+            flowchart.SendFungusMessage(sendMessage);
+            flowchart.SendFungusMessage(sendMessage2);
+        }
+    }
 }
